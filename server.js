2bdc3d7e34a5d17e38e056635dd30ccf7083eb20
Ryan Gabriel <rggs1@aluno.ifnmg.edu.br>
2025-09-18 17:17:04 +0000

    Atualizado

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 import express from "express";   
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
 const app = express();              
 const port = 3000;                
 
-app.get("/", (req, res) => {        
+app.get("/", async (req, res) => {
+  const db = new Pool({  
+  connectionString: process.env.URL_BD,
+});
+
+let dbStatus = "ok";
+try {
+  await db.query("SELECT 1");
+} catch (e) {
+  dbStatus = e.message;
+}        
   console.log("Rota GET / solicitada");
   res.json({
 		message: "API para atividade",      
-    author: "Ryan Gabriel Gonçalves Silva",    
+    author: "Ryan Gabriel Gonçalves Silva", 
+    statusBD: dbStatus  
   });
 });
 
